e32846797c9d88b12ddfac6e7406dd43ea771b49
AppleSeed <everlastingfriendship194@gmail.com>
2024-06-20 00:15:00 +0900

    feat : credit card payment implementation

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import login_view,get_ticket,purchase_ticket
+from .views import login_view,get_ticket,purchase_ticket,calc_time,purchase_credit_ticket
 
 urlpatterns = [
     path('login', login_view, name='login'),
     path('get-ticket', get_ticket, name='get_ticket'),
     path('purchase-ticket', purchase_ticket, name='purchase_ticket'),
-    
+    path('calc-time', calc_time, name='calc_time'),
+    path('purchase-credit-ticket', purchase_credit_ticket, name='purchase_credit_ticket'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,6 +3,20 @@ from django.shortcuts import render
 import ntplib
 import requests
 import re
+import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+import os
+from django.conf import settings
+import time
+
+
 
 
 
@@ -98,6 +112,81 @@ def get_ticket(request):
     return JsonResponse(response_data, safe=False)
 
 
+def calc_time(request):
+    if request.method == 'POST':
+        _ntpServer = 'ntp1.jst.mfeed.ad.jp'
+        data = json.loads(request.body)
+        _scheduledTime = data['ticketTime']
+        _beforeTime = data['beforeTime']
+        scheduledTime = datetime.datetime(
+                                    year        = int(_scheduledTime[0:4]),
+                                    month       = int(_scheduledTime[5:7]),
+                                    day         = int(_scheduledTime[8:10]),
+                                    hour        = int(_scheduledTime[11:13]),
+                                    minute      = int(_scheduledTime[14:16]),
+                                    second      = int(_scheduledTime[17:19]),
+                                    tzinfo      = None
+                                )
+        scheduledTimeUnix = datetime.datetime.timestamp(scheduledTime)
+        scheduledTimeBeforeUnix = datetime.datetime.timestamp(scheduledTime - datetime.timedelta(seconds=_beforeTime))
+        ntpTimeUnix = getNtpTimeUnix(_ntpServer)
+
+        secondDiff = float(scheduledTimeBeforeUnix) - float(ntpTimeUnix)
+        response_data = {'data': secondDiff if secondDiff > 0 else 0}
+        return JsonResponse(response_data, safe=False)
+
+def purchase_credit_ticket(request):
+    data = json.loads(request.body)
+    email = data['email']
+    password = data['password']
+    event_url = data['event_url']
+    ticket_id = data['ticket_id']
+    ticket_cnt = data['ticket_cnt']
+    security_code = data['security_code']
+    # webdriver_path = os.path.join(settings.BASE_DIR, 'chromedriver.exe')
+
+    driver = webdriver.Chrome()
+    try:
+        # Open the webpage
+        driver.get('https://t.livepocket.jp/login?acroot=header-new_p_u_nl')
+
+        wait = WebDriverWait(driver, 10)
+        email_field = wait.until(EC.presence_of_element_located((By.ID, 'email')))
+        email_field.send_keys(email)
+        password_field = wait.until(EC.presence_of_element_located((By.ID, 'password')))
+        password_field.send_keys(password)
+
+        login_button = driver.find_element(By.CSS_SELECTOR, '.btn-login-pc')
+        login_button.click()
+        time.sleep(5)
+        driver.get(event_url)
+        select_element = wait.until(EC.presence_of_element_located((By.ID, 'ticket-' + str(ticket_id))))
+        select = Select(select_element)
+        select.select_by_visible_text(str(ticket_cnt) +'枚')
+        time.sleep(5)
+        confirm_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'p.button.register.center-block.btn-procedure-pc-only button.register_input_submit.register_input_submit_pink')))
+        confirm_button.click()
+        checkbox = wait.until(EC.element_to_be_clickable((By.ID, 'agreement_check_lp')))
+        checkbox.click()
+        check_button = wait.until(EC.element_to_be_clickable((By.ID, 'submit-btn')))
+        check_button.click()
+        # driver.find_element_by_tag_name('body').send_keys(Keys.ENTER)
+        time.sleep(3)
+        alert = driver.switch_to.alert
+        alert.accept()
+        security_code_field = wait.until(EC.presence_of_element_located((By.ID, 'securityCode')))
+        security_code_field.send_keys(security_code)
+        buy_button = wait.until(EC.element_to_be_clickable((By.ID, 'exec-button')))
+        buy_button.click()
+        time.sleep(5)
+        buy_button = wait.until(EC.element_to_be_clickable((By.ID, 'exec-button')))
+        buy_button.click()
+        time.sleep(20)
+    finally:
+        print('end')
+
+    return JsonResponse({'confirmation': 'text'})
+
 def purchase_ticket(request):
     if request.method == 'POST':
         with requests.session() as session:
@@ -113,13 +202,13 @@ def purchase_ticket(request):
 
             requestUrl = 'https://t.livepocket.jp/api/sessions/create?mytimestamp=' + str(int(getNtpTimeUnix(_ntpServer)*1000))
 
-            requestDatadata = {
+            requestData = {
                 'login': data['email'],
                 'password': data['password'],
                 'login_password': data['email'] + '&' + data['password'],
             }
             res = session.get(requestUrl, headers=header)
-            res = session.post(requestUrl, data=requestDatadata, headers=header, cookies=res.cookies)
+            res = session.post(requestUrl, data=requestData, headers=header, cookies=res.cookies)
             requestUrl = 'https://t.livepocket.jp/purchase?type=new'
             res = session.get(requestUrl, headers=header)
             res = session.get(requestUrl, headers=header, cookies=res.cookies)
@@ -149,7 +238,10 @@ def purchase_ticket(request):
             reservedSessionId = locationPart[3]
             onetimeToken = locationPart[5]
             if payment_method == '0':
-                requestUrl = 'https://t.livepocket.jp/api/tickets/purchase?mytimestamp=' + str(int(getNtpTimeUnix(_ntpServer)*1000))
+                if data['security_code']:
+                    requestUrl = 'https://t.livepocket.jp/api/tickets/prepare_purchase?mytimestamp=' + str(int(getNtpTimeUnix(_ntpServer)*1000))
+                else:
+                    requestUrl = 'https://t.livepocket.jp/api/tickets/purchase?mytimestamp=' + str(int(getNtpTimeUnix(_ntpServer)*1000))
                 parameterPaymentType = 'credit'
             else:
                 requestUrl = 'https://t.livepocket.jp/api/tickets/prepare_purchase?mytimestamp=' + str(int(getNtpTimeUnix(_ntpServer)*1000))
@@ -169,29 +261,47 @@ def purchase_ticket(request):
                 'fan_club': 'null',
                 'use_discount_id': '',
                 'use_discount_code_id': '',
-                'security_code': ''
             }
             if payment_method == '0':
-                print(payment_method)
-                # requestData.update({"security_code": _securityCode})
+                if(data['security_code']):
+                    security_code = data['security_code']
+                    requestData.update({"security_code": security_code})
             else:
                 selected_cvs_code = data['selected_cvs_code']
                 requestData.update({"selected_cvs_code": selected_cvs_code})
             res = session.post(requestUrl, data=requestData, headers=header, cookies=res.cookies)
             jsonData = json.loads(res.text)
+            if(jsonData['success'] == False):
+                response_data = {"status":'fail',"data":jsonData}
+                return JsonResponse(response_data, safe=False)
             orderId = jsonData['result']['order_id']
             onetimeToken = jsonData['result']['onetime_token_value']
             if payment_method == '0':
-                requestUrl = 'https://t.livepocket.jp/purchase/enquate'
-                requestData = {
-                    'reserve_session_id': reservedSessionId,
-                    'order_id': orderId,
-                    'onetime_token_name': 'buy_ticket',
-                    'onetime_token_value': onetimeToken,
-                }
-                res = session.post(requestUrl, data=requestData, headers=header, cookies=res.cookies)
-                
-                response_data = {"data":res.text}
+                if data['security_code']:
+                    requestUrl = 'https://t.livepocket.jp/purchase/credit_confirm'
+                    requestData = {
+                        'id' : eventId,
+                        'reserved_session_id' : reservedSessionId,
+                        'payment_method_type' :'',
+                        'note':'',
+                        'order_id': orderId,
+                        'onetime_token_name': 'buy_ticket',
+                        'onetime_token_value': onetimeToken,
+                    }
+                    res = session.post(requestUrl, data=requestData, headers=header, cookies=res.cookies)
+                    print(res.text)
+                    response_data = {"data":'samples'}
+                else:
+                    requestUrl = 'https://t.livepocket.jp/purchase/enquate'
+                    requestData = {
+                        'reserve_session_id': reservedSessionId,
+                        'order_id': orderId,
+                        'onetime_token_name': 'buy_ticket',
+                        'onetime_token_value': onetimeToken,
+                    }
+                    res = session.post(requestUrl, data=requestData, headers=header, cookies=res.cookies)
+                    
+                    response_data = {"data":res.text}
             else:
                 requestUrl = 'https://t.livepocket.jp/purchase/cvs_confirm'
                 selected_cvs_code = data['selected_cvs_code']
@@ -205,20 +315,25 @@ def purchase_ticket(request):
                     'onetime_token_name': 'buy_ticket',
                     'onetime_token_value': onetimeToken,
                 }
-                res = session.post(requestUrl, data=data, headers=header, cookies=res.cookies)
+                res = session.post(requestUrl, data=requestData, headers=header, cookies=res.cookies)
                 requestUrl = 'https://t.livepocket.jp/purchase/form_redirect?onetime_token_name=buy_ticket&onetime_token_value=' + onetimeToken
                 res = session.get(requestUrl, headers=header, cookies=res.cookies)
-                requestUrl = 'https://t.livepocket.jp/purchase/enquate?order_id=' + orderId + '&onetime_token_name=buy_ticket&onetime_token_value=' + onetimeToken
-                res = session.get(requestUrl, headers=header, cookies=res.cookies)
-                # print(res.text)
                 if res.history:
                     for resp in res.history:
                         print(f"Redirected from {resp.url} to {resp.headers['Location']}")
                     print(f"Final destination: {res.url}")
                 else:
                     print("No redirects")
-                requestUrl = 'https://t.livepocket.jp/purchase/complete?order_id=' + orderId
-                res = session.get(requestUrl, headers=header, cookies=res.cookies)
+                # requestUrl = 'https://t.livepocket.jp/purchase/enquate?order_id=' + orderId + '&onetime_token_name=buy_ticket&onetime_token_value=' + onetimeToken
+                # res = session.get(requestUrl, headers=header, cookies=res.cookies)
+                # # if res.history:
+                # #     for resp in res.history:
+                # #         print(f"Redirected from {resp.url} to {resp.headers['Location']}")
+                # #     print(f"Final destination: {res.url}")
+                # # else:
+                # #     print("No redirects")                
+                # requestUrl = 'https://t.livepocket.jp/purchase/complete?order_id=' + orderId
+                # res = session.get(requestUrl, headers=header, cookies=res.cookies)
                 response_data = {"data":res.text}
     else:
         response_data = {'msg': 'This endpoint accepts only POST requests.'}
